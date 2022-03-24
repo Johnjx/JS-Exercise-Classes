dddf0c6e0f36195c5149d6012ac3c1c8f3048564
John Ruddock <john.jxr@pm.me>
2022-03-24 12:56:37 -0400

    completed task 6, final task: created ProjectManager subclass on Instructor

--- index.js
+++ index.js
@@ -200,13 +200,23 @@ class Student extends Lambdasian {
         + `favInstructor`: i.e. Sean
     - Its constructor calls the parent constructor passing to it what it needs.
     - The constructor should also initialize `gradClassName` and `favInstructor` properties on the instance.
-    - ProjectManager instances have the following methods:
+    - ProjectManager instances have the following mjethods:
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
 
 class ProjectManager extends Instructor {
-   
+   constructor({name, age, location, specialty, favLanguage, catchPhrase, gradClassName, favInstructor}){
+     super({name, age, location, specialty, favLanguage, catchPhrase});
+     this.gradClassName = gradClassName;
+     this.favInstructor = favInstructor;
+   }
+   standUp(channel){
+      return `${this.name} announces to ${channel}, @${channel} standy times!`;
+   }
+   debugsCode(student, subject){
+      return `${this.name} debugs ${student.name}'s code on ${subject}`;
+   }
 }
 
 /*
