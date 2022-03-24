1e3053405902b1f59661d401dc04d4f736cdff3a
John Ruddock <john.jxr@pm.me>
2022-03-24 10:11:02 -0400

    completed task 1: created Person class

--- index.js
+++ index.js
@@ -44,7 +44,24 @@ class Airplane {
 */
 
 class Person {
-  
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+    eat (someFood) {
+      if (this.stomach.length < 10){
+      this.stomach.push(someFood);
+      } else {
+        return 'Stomach Full!'
+      }
+    }
+    poop() {
+       this.stomach = [];
+      }
+    toString() {
+      return `${this.name}, ${this.age}`;
+    }  
 }
 
 /*
